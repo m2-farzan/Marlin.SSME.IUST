c91157aa940c0c59efcb8fa948e0e78f90e48cae
erfan-mtr <erfanmahmoodtabar@yahoo.com>
2019-05-24 19:40:14 +0430

    Filament diameter changed.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1,4 +1,4 @@
-/**
+﻿/**
  * Marlin 3D Printer Firmware
  * Copyright (C) 2016 MarlinFirmware [https://github.com/MarlinFirmware/Marlin]
  *
@@ -149,7 +149,7 @@
 #define EXTRUDERS 1
 
 // Generally expected filament diameter (1.75, 2.85, 3.0, ...). Used for Volumetric, Filament Width Sensor, etc.
-#define DEFAULT_NOMINAL_FILAMENT_DIA 3.0 // ask ?
+#define DEFAULT_NOMINAL_FILAMENT_DIA 1.75 // ask ?
 
 // For Cyclops or any "multi-extruder" that shares a single nozzle.
 //#define SINGLENOZZLE
